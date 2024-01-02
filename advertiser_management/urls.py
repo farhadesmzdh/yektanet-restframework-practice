196d6f77a16f124f8472cdfb5e8382b77fefd8d3
farhadesmzdh <farhadesmzdh@gmail.com>
2024-01-02 15:05:56 +0330

    push django project on new repository

--- advertiser_management/urls.py
+++ advertiser_management/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import AdsView, AdClickView, AddAdView, ClickCountView, RatioClickByView, TimeBetweenClickView
 
+
 urlpatterns = [
     path('ads/', AdsView.as_view(), name='ads_view'),
     path('ad_click/<int:ad_id>/', AdClickView.as_view(), name='ad_click'),
