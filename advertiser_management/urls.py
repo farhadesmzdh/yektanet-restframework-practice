607efaaf7b6a0e9daf5c2c7532e122bfe0c6a437
farhadesmzdh <farhadesmzdh@gmail.com>
2024-01-03 10:41:37 +0330

    url update

--- advertiser_management/urls.py
+++ advertiser_management/urls.py
@@ -3,12 +3,12 @@ from .views import AdClickView, AdsApiView, ClickCountApiView, RatioClickByApiVi
 
 
 urlpatterns = [
-    path('ads/', AdsApiView.as_view(), name='ads_view'),
-    path('ad_click/<int:ad_id>/', AdClickView.as_view(), name='ad_click'),
-    path('add_ad/', AdsApiView.as_view(), name='add_ad'),
-    path('click_count/', ClickCountApiView.as_view(), name='click_count'),
-    path('ratio_click_view/', RatioClickByApiView.as_view(), name='ratio_click_view'),
-    path('time_between_click_view/', TimeBetweenClickApiView.as_view(), name='time_between_click_view'),
+    path('api/ads/', AdsApiView.as_view(), name='ads_view'),
+    path('api/ad_click/<int:ad_id>/', AdClickView.as_view(), name='ad_click'),
+    path('api/add_ad/', AdsApiView.as_view(), name='add_ad'),
+    path('api/click_count/', ClickCountApiView.as_view(), name='click_count'),
+    path('api/ratio_click_view/', RatioClickByApiView.as_view(), name='ratio_click_view'),
+    path('api/time_between_click_view/', TimeBetweenClickApiView.as_view(), name='time_between_click_view'),
 ]
 
 
